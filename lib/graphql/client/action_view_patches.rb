226f73417820cee7a0b79582d325cfe1fe31257c
Pat Nakajima <patnakajima@gmail.com>
2017-03-31 19:54:04 -0700

    Start on erb magic casting

---
+++ lib/graphql/client/action_view_patches.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# Monkeypatch to allow magic GraphQL casting
+ActionView::Template::Handlers::ERB.class_eval do
+  def call(template)
+    if template.source.encoding_aware?
+      # First, convert to BINARY, so in case the encoding is
+      # wrong, we can still find an encoding tag
+      # (<%# encoding %>) inside the String using a regular
+      # expression
+      template_source = template.source.dup.force_encoding("BINARY")
+
+      erb = template_source.gsub(ActionView::Template::Handlers::ERB::ENCODING_TAG, '')
+      encoding = $2
+
+      erb.force_encoding valid_encoding(template.source.dup, encoding)
+
+      # Always make sure we return a String in the default_internal
+      erb.encode!
+    else
+      erb = template.source.dup
+    end
+
+    require 'byebug'
+    byebug
+
+    self.class.erb_implementation.new(
+      erb,
+      :filename => template, # We need this passed as a property to figure out magic modules
+      :escape => (self.class.escape_whitelist.include? template.mime_type),
+      :trim => (self.class.erb_trim_mode == "-")
+    ).src
+  end
+end
--- lib/graphql/client/erubis_enhancer.rb
+++ lib/graphql/client/erubis_enhancer.rb
@@ -14,7 +14,29 @@ module GraphQL
     module ErubisEnhancer
       # Internal: Extend Erubis handler to simply ignore <%graphql sections.
       def convert_input(src, input)
-        input = input.gsub(/<%graphql/, "<%#")
+        graphql, _ = GraphQL::Client::ViewModule.extract_graphql_section(input)
+
+        # Get the name of the fragment
+        #
+        # So `fragment Pat on User` would return "Pat"
+        fragment_name = graphql.match(/fragment ([A-Z]\w+) on/).try :[], 1
+
+        # Convert it to a local we'll use
+        local_name = fragment_name.underscore
+
+        # Get the namespace
+        const_name = @filename.inspect
+          .gsub(/^app\//, "")
+          .gsub(/\.html\.erb$/, "")
+          .camelize
+
+        input = input.gsub /<%graphql/, <<-ERB
+        <%
+          raise ArgumentError, "This template must be passed `#{local_name}`" unless local_assigns[:#{local_name}]
+          #{local_name} = #{const_name}::#{fragment_name}.new(#{local_name})
+        %>
+        ERB
+
         super(src, input)
       end
     end

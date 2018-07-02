8f8bae51a8a7917587d51b29328d47a7a4e19b0a
Marko Satek <marko.satek@storyful.com>
2018-07-02 14:21:58 +0100

    generated request body in execution context

--- test/test_http.rb
+++ test/test_http.rb
@@ -4,14 +4,20 @@ require "graphql/client/http"
 require "minitest/autorun"
 
 class TestHTTP < MiniTest::Test
+  CONTEXT_USER = Minitest::Mock.new
+
   SWAPI = GraphQL::Client::HTTP.new("https://mpjk0plp9.lp.gql.zone/graphql") do
-    def headers(_context)
+    def headers(context)
+      request_json_fields = Hash(JSON.parse(context["request_body"])).keys
+      CONTEXT_USER.use_body(request_json_fields)
       { "User-Agent" => "GraphQL/1.0" }
     end
   end
 
   def test_execute
     skip "TestHTTP disabled by default" unless __FILE__ == $PROGRAM_NAME
+
+    CONTEXT_USER.expect(:use_body, nil, [%w[query variables operationName]])
 
     document = GraphQL.parse(<<-'GRAPHQL')
       query getCharacter($id: ID!) {
@@ -33,5 +39,6 @@ class TestHTTP < MiniTest::Test
     }
     actual = SWAPI.execute(document: document, operation_name: name, variables: variables)
     assert_equal(expected, actual)
+    assert(CONTEXT_USER.verify)
   end
 end
